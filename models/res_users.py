63df630cf542644d16f6e31c06d2fc3e48871d38
wahhid <wahhid@gmail.com>
2018-03-27 21:02:16 +0700

    Initial commit

---
+++ models/res_users.py
@@ -0,0 +1,7 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError, UserError, Warning
+
+class ResUser(models.Model):
+    _inherit = 'res.users'
+
+    pos_config_id = fields.Many2one('pos.config','Point of Sale')
